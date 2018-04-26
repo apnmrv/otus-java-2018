2beecdf59bb3c581c2e0fc9a2af10de9eac1e160
Andrey Ponomarev <apnmrv@gmail.com>
2018-04-26 15:05:26 +0300

    новый файл:    L02.1-StringTutor/pom.xml 	удалено:       L02.1-StringTutor/src/java/ru/otus/l021/Main.java 	новый файл:    L02.1-StringTutor/src/main/java/ru/otus/StringTutor.java 	новый файл:    L02.1-StringTutor/src/test/java/ru/otus/AppTest.java

---
+++ L02.1-StringTutor/src/main/java/ru/otus/StringTutor.java
@@ -0,0 +1,13 @@
+package ru.otus;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringTutor
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
