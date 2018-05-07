7c7d9afc01a700d6fd23497597aacd95a729f75c
Andrey Ponomarev <apnmrv@gmail.com>
2018-05-07 18:09:00 +0300

    новый файл:    L02.3-ObjectSizeWithAgent/src/main/java/ru/otus/java/AgentCounter.java 	удалено:       L02.3-ObjectSizeWithAgent/src/main/java/ru/otus/java/MaxOttoVonStierlitz.java 	новый файл:    L02.3-ObjectSizeWithAgent/src/test/java/ru/otus/java/MaxOttoVonStierlitzTest.java 	удалено:       L02.3-ObjectSizeWithAgent/src/test/java/ru/otus/java/MaxOttoVonStierlitzTester.java 	переименовано: L02.3-ObjectSizeWithAgent/src/main/java/ru/otus/java/Printer.java -> L02.3-ObjectSizeWithAgent/tmp/Printer.java 	изменено:      L02.3-ObjectSizeWithAgent/pom.xml 	изменено:      L02.3-ObjectSizeWithAgent/src/main/java/ru/otus/java/AgentCounter.java 	изменено:      L02.3-ObjectSizeWithAgent/src/main/java/ru/otus/java/App.java 	изменено:      L02.3-ObjectSizeWithAgent/src/main/resources/META-INF/MANIFEST.MF 	изменено:      L02.3-ObjectSizeWithAgent/src/test/java/ru/otus/java/MaxOttoVonStierlitzTest.java
    	L02.3-ObjectSizeWithAgent/tmp/_App.java

---
+++ L02.3-ObjectSizeWithAgent/src/main/java/ru/otus/java/AgentCounter.java
@@ -0,0 +1,81 @@
+package ru.otus.java;
+
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.*;
+import java.util.*;
+
+public class MaxOttoVonStierlitz {
+
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args,
+                               Instrumentation inst) {
+
+        MaxOttoVonStierlitz.instrumentation = inst;
+        System.out.println("Hello! I`m Stierlitz.");
+    }
+
+    /** Get object size */
+    public static long estimateSizeOf(Object obj){
+        if (instrumentation == null) {
+            throw new IllegalStateException(
+                    "Stierlitz's not initialized");
+        }
+        return instrumentation.getObjectSize(obj);
+    }
+
+    /**Iterating through object's components*/
+    public static long estimateFullSizeOf(Object obj){
+        Map checked = new HashMap();
+        Stack stack = new Stack();
+        stack.push(obj);
+
+        long result = 0;
+
+        do {
+            result += internalSizeOf(stack.pop(), stack, checked);
+        } while (!stack.isEmpty());
+        return result;
+    }
+
+    private static long internalSizeOf(
+            Object obj, Stack stack, Map checked) {
+        if (obj == null || checked.containsKey(obj)) {
+            return 0;
+        }
+
+        Class clazz = obj.getClass();
+        if (clazz.isArray()) {
+            addArrayElementsToStack(clazz, obj, stack);
+        } else {
+            // add all non-primitive fields to the stack
+            while (clazz != null) {
+                Field[] fields = clazz.getDeclaredFields();
+                for (Field field : fields) {
+                    if (!Modifier.isStatic(field.getModifiers())
+                            && !field.getType().isPrimitive()) {
+                        field.setAccessible(true);
+                        try {
+                            stack.add(field.get(obj));
+                        } catch (IllegalAccessException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                }
+                clazz = clazz.getSuperclass();
+            }
+        }
+        checked.put(obj, null);
+        return estimateSizeOf(obj);
+    }
+
+    private static void addArrayElementsToStack(
+            Class clazz, Object obj, Stack stack) {
+        if (!clazz.getComponentType().isPrimitive()) {
+            int length = Array.getLength(obj);
+            for (int i = 0; i < length; i++) {
+                stack.add(Array.get(obj, i));
+            }
+        }
+    }
+}
---
+++ L02.3-ObjectSizeWithAgent/src/test/java/ru/otus/java/MaxOttoVonStierlitzTest.java
@@ -0,0 +1,16 @@
+package ru.otus.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaxOttoVonStierlitzTester {
+    /**
+     * Rigorous Test
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
